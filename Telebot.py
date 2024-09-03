b3c94a9b151f1a9de20b2f3eb96acda0aca23634
MillionKiwi <leehh41563636@gmail.com>
2024-09-04 00:16:35 +0900

    add get ip function

--- Telebot.py
+++ Telebot.py
@@ -1,12 +1,17 @@
 import telepot
 import time
 import os
+import requests
+import subprocess
+import json
 
+#telegram msg handle
 def handle(msg):
 	chat_id = msg['chat']['id']
 	command = msg['text']
 	
-	print('Command:  %s' , command)
+	
+	print('Command: ', command)
 	
 	if command == '/start':
 		bot.sendMessage(chat_id, "This bot is for detecting North Korea Trash-landen Balloons.\n ")
@@ -16,10 +21,38 @@ def handle(msg):
 		os.system("python webcam.py")
 		print("webcam run")
 		bot.sendMessage(chat_id, "A suspected balloon object has been detected!\n ")
+		bot.sendMessage(chat_id, "location: " + str(latitude) + ", " + str(longitude) + "\nCity: " + city)
 		bot.sendPhoto(chat_id, photo = open("photo.jpg", 'rb'))
 
+#get location based on ip	
+def getLocationIpstack(api_key):
+    try:
+        result = subprocess.run("curl ifconfig.co", shell=True, capture_output=True, text=True)
+        ip = result.stdout.strip()
+        
+        print(ip)
+        
+        url = f"http://api.ipstack.com/{ip}?access_key={api_key}"
+        response = requests.get(url)
+        data = json.loads(response.text)
+        
+        global latitude
+        global longitude
+        global city
+
+        latitude = data["latitude"]
+        longitude = data["longitude"]
+        city = data["city"]
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
 token = "7340681503:AAGmzgZpPoStNrMHF2Bt536Bvs8o3dKNJ6o"
+api_key = "a0a0f44a512653fae3fe2c9eb83d8e12"
 bot = telepot.Bot(token)
+
+getLocationIpstack(api_key)
+
 
 bot.message_loop(handle)
 print ('I am listening..')
